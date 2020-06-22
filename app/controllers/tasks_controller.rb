0e82a00bc871cd2111db7bee8abc6e90b62a6f48
namatamerikuma <lshengtianmu55@gmail.com>
2020-06-22 19:27:45 +0900

    ソート機能の追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
 
   def index
     @q = current_user.tasks.ransack(params[:q])
-  	@tasks = @q.result(distinct: true).recent
+    @tasks = @q.result(didtinct: true)
   end
 
   def show
